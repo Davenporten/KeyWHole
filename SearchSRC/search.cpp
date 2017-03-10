e95438b60d6547abe60a3028ad9d41c567e1eb57
RClinton <davenporten@gmail.com>
2017-03-10 12:28:33 -0700

    Find match without progressing

--- SearchSRC/search.cpp
+++ SearchSRC/search.cpp
@@ -1,32 +1,40 @@
 #include "search.h"
 
-void preprocess(char* text, char* pattern) {
+void preprocess(string text, string pattern) {
 
 
 }
 
-void search(char* text, char* pattern) {
+void search(string text, string pattern) {
 	// The end condition to signal the text is done
-	int text_end = strlen(text) - 1;
+	int text_end = text.size() - 1;
 	
-	// Current char being considered in text
+	// Current string being considered in text
 	// Position the text and pattern over each other
-	int i_text = strlen(pattern) - 1;
-	char* found = "";
-	for(int i = strlen(pattern)-1; i >= 0; i--) {
+	int i_text = pattern.size() - 1;
+	string found = "";
+	for(int i = pattern.size()-1; i >= 0; i--) {
 		if (text[i_text] == pattern[i]) {
 			i_text--;
-			found += to_string(i) + '#'; 
+			if (!i) {
+				found += to_string(i)+'#';
+				i = pattern.size() - 1;
+				// TODO: Need to add something to set i_text to the next appropriate value
+			}
 			continue;
 		}
+
+
+		// This should always go at the end of the loop
+		if (i_text >= text_end) break;
 	}
 	cout << found;
 }
 
 
 int main(){
-	char* text = "Hey you over there, hey!";
-    char* pattern = "Hey";
+	string text = "Hey you over there, hey!";
+    string pattern = "Hey";
 	search(text, pattern);
 	return 0;
 }
--- SearchSRC/search.h
+++ SearchSRC/search.h
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-// Used for indexing into the tables related to specific chars
+// Used for indexing into the tables related to specific strings
 enum alphabeta {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, ZERO};
 
 
-void preprocess(char* text, char* pattern);
-void search(char* text, char* pattern);
+void preprocess(string text, string pattern);
+void search(string text, string pattern);
 
 #endif
