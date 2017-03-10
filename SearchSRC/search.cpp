fc7595c739e0f2ca5a005b2c282d5fed82bf4630
RClinton <davenporten@gmail.com>
2017-03-10 10:35:33 -0700

    Figuring out which language

--- SearchSRC/search.cpp
+++ SearchSRC/search.cpp
@@ -5,29 +5,29 @@ void preprocess(char* text, char* pattern) {
 
 }
 
-void search(char* text, char* pattern, char* found) {
+void search(char* text, char* pattern) {
 	// The end condition to signal the text is done
 	int text_end = strlen(text) - 1;
 	
 	// Current char being considered in text
 	// Position the text and pattern over each other
 	int i_text = strlen(pattern) - 1;
+	char* found = "";
 	for(int i = strlen(pattern)-1; i >= 0; i--) {
 		if (text[i_text] == pattern[i]) {
 			i_text--;
-			printf("Hey\n");
+			found += to_string(i) + '#'; 
 			continue;
 		}
 	}
-
+	cout << found;
 }
 
 
 int main(){
-	char* test = "";
 	char* text = "Hey you over there, hey!";
     char* pattern = "Hey";
-	search(text, pattern, test);
+	search(text, pattern);
 	return 0;
 }
 
